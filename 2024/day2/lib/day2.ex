0c6b51b9fa692dc7c084cb1589bd07904481e1d1
Garrett Phelps <garrphelps@gmail.com>
2024-12-02 15:40:14 -0600

    Day 2

--- 2024/day2/lib/day2.ex
+++ 2024/day2/lib/day2.ex
@@ -1,11 +1,66 @@
 defmodule Day2 do
   @moduledoc """
-  Documentation for `Day2`.
-  """
+  Validate that given rows of reports, where each report
+  is an integer, all rows are either consistenty increase/decreasing.
+  Any two adjacent reports should differ by atleast 1 but no more than 3
 
+
+  Ex.
+  7 6 4 2 1
+  1 2 7 8 9
+  9 7 6 2 1
+  1 3 2 4 5
+  8 6 4 4 1
+  1 3 6 7 9
+  """
   @doc """
   """
-  def hello do
-    :world
+
+  defmodule Report do
+    @enforce_keys [:values]
+    defstruct values: []
+
+    @type t() :: %__MODULE__{
+      values: list(integer())
+    }
+
+    @spec safe?(Day2.Report.t(), integer()) :: boolean()
+    def safe?(report, 0) do
+      chunks = Enum.chunk_every(report.values, 2, 1, :discard)
+
+      increasing_safety_levels = chunks
+        |> Enum.map(fn [a, b] -> a < b end)
+      decreasing_safety_levels = chunks
+        |> Enum.map(fn [a, b] -> a > b end)
+      within_3_safety_levels = chunks
+        |> Enum.map(fn [a, b] -> abs(b - a) <= 3 end)
+
+      (Enum.all?(increasing_safety_levels) or Enum.all?(decreasing_safety_levels))
+        and Enum.all?(within_3_safety_levels)
+    end
+
+    def safe?(report, dampening_level) do
+      safe_list = for n <- 0..length(report.values) do
+        modified_report = %Report{values: List.delete_at(report.values, n)}
+        Report.safe?(modified_report, dampening_level - 1)
+      end
+
+      if length(safe_list) == 0 do
+        true
+      else
+        Enum.any?(safe_list);
+      end
+    end
   end
+
+  @spec create_reports(String.t()) :: list(Report.t())
+  def create_reports(input_string) when is_binary(input_string) do
+    String.split(input_string, ~r/\r?\n/, trim: true)
+    |> Enum.map(fn report_string ->
+        integer_list = String.split(report_string)
+          |> Enum.map(fn val -> String.to_integer(val) end)
+        %Report{values: integer_list}
+      end)
+  end
+
 end
--- 2024/day2/test/day2_test.exs
+++ 2024/day2/test/day2_test.exs
@@ -2,7 +2,86 @@ defmodule Day2Test do
   use ExUnit.Case
   doctest Day2
 
-  test "greets the world" do
-    assert Day2.hello() == :world
+  test "empty report are safe" do
+    assert Day2.Report.safe?(%Day2.Report{values: []}, 0) === true
+  end
+
+  test "single value report is safe" do
+    assert Day2.Report.safe?(%Day2.Report{values: [1]}, 0) === true
+  end
+
+  test "report is safe if they are all increasing" do
+    assert Day2.Report.safe?(%Day2.Report{values: [1, 2, 3, 4, 5]}, 0) === true
+  end
+
+  test "report is safe if they are all decreasing" do
+    assert Day2.Report.safe?(%Day2.Report{values: [5, 4, 3, 2, 1]}, 0) === true
+  end
+
+  test "report is not safe if there is a repeating adjacent value" do
+    assert Day2.Report.safe?(%Day2.Report{values: [5, 4, 3, 3, 2, 1]}, 0) === false
+  end
+
+  test "report is not safe if adjacent value exceeds 3" do
+    assert Day2.Report.safe?(%Day2.Report{values: [8, 4, 3, 2, 1,]}, 0) === false
+  end
+
+  test "report is not safe if it does not strictly increase/decrease" do
+    assert Day2.Report.safe?(%Day2.Report{values: [5, 4, 1, 2, 3]}, 0) === false
+  end
+
+  test "report is safe with dampening with a single unsafe inc/dec level" do
+    assert Day2.Report.safe?(%Day2.Report{values: [5, 1, 2, 3]}, 1) === true
+  end
+
+  test "report is safe with dampening with a single unsafe >3 level" do
+    assert Day2.Report.safe?(%Day2.Report{values: [1, 5, 3]}, 1) === true
+  end
+
+  test "report is not safe with dampening with a multiple unsafe levels" do
+    assert Day2.Report.safe?(%Day2.Report{values: [5, 4, 1, 2, 3]}, 1) === false
+  end
+
+  test "create_report creates empty reports if empty string" do
+    assert Day2.create_reports("") === []
+  end
+
+  test "create_report creates 1 report for a single row" do
+    assert Day2.create_reports("1 2 3 4 5") ===
+        [%Day2.Report{values: [1, 2, 3, 4, 5]}]
+  end
+
+  test "create_report creates multiple reports for a multiple rows" do
+    assert Day2.create_reports("""
+      1 2 3 4 5
+      5 4 3 2 1
+      0 0 0 0 0
+    """) === [
+      %Day2.Report{values: [1, 2, 3, 4, 5]},
+      %Day2.Report{values: [5, 4, 3, 2, 1]},
+      %Day2.Report{values: [0, 0, 0, 0, 0]}
+    ]
+  end
+
+  test "day2 part 1 integration" do
+    case File.read("test/test_data.txt") do
+      {:ok, content} ->
+        assert Day2.create_reports(content)
+          |> Enum.count(fn report -> Day2.Report.safe?(report, 0) end) == 242
+
+      {:error, reason} ->
+        raise "Error reading file: #{reason}"
+    end
+  end
+
+  test "day2 part 2 integration" do
+    case File.read("test/test_data.txt") do
+      {:ok, content} ->
+        assert Day2.create_reports(content)
+          |> Enum.count(fn report -> Day2.Report.safe?(report, 1) end) == 311
+
+      {:error, reason} ->
+        raise "Error reading file: #{reason}"
+    end
   end
 end
