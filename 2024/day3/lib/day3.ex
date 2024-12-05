82baad31c7ed432db9421f94e11cb0b3caba47eb
Garrett Phelps <garrphelps@gmail.com>
2024-12-05 13:01:28 -0600

    Day3 - Add regex for parsing mul directives

--- 2024/day3/lib/day3.ex
+++ 2024/day3/lib/day3.ex
@@ -4,15 +4,24 @@ defmodule Day3 do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day3.hello()
-      :world
-
   """
-  def hello do
-    :world
+  @spec find_mul(String.t()) :: list(String.t())
+  def find_mul(input_string) do
+    Regex.scan(~r/mul\(-?[0-9]{1,3},-?[0-9]{1,3}\)/, input_string)
+    |> List.flatten()
+  end
+
+  @doc """
+  """
+  @spec mul(list(String.t())) :: integer()
+  def mul(_mul_list) do
+    # mul
+    #   |> Enum.map(fn [a, b] ->
+    #     Regex.scan(~r/mul\(-?[0-9]{1,3},-?[0-9]{1,3}\)/, input_string)
+    #     Regex.scan(~r/mul\(-?[0-9]{1,3},-?[0-9]{1,3}\)/, input_string)
+    #     a < b
+    #   end)
+    # |> List.flatten()
+    0
   end
 end
--- 2024/day3/test/day3_test.exs
+++ 2024/day3/test/day3_test.exs
@@ -2,7 +2,38 @@ defmodule Day3Test do
   use ExUnit.Case
   doctest Day3
 
-  test "greets the world" do
-    assert Day3.hello() == :world
+  test "find_mult return zero with empty string" do
+    assert length(Day3.find_mul("")) == 0
+  end
+
+  test "find_mult return zero with malformed input" do
+    assert length(Day3.find_mul("ul(1,2)")) == 0
+    assert length(Day3.find_mul("mul(--1,2)")) == 0
+    assert length(Day3.find_mul("mul(*1,2*)")) == 0
+    assert length(Day3.find_mul("(1,2)")) == 0
+    assert length(Day3.find_mul("mul( 1, 2)")) == 0
+    assert length(Day3.find_mul("mul(1, 2 )")) == 0
+    assert length(Day3.find_mul("mul( 1, 2 )")) == 0
+  end
+
+  test "find_mult returns all mults found" do
+    mul1 = Day3.find_mul("mul(1,2)")
+    assert length(mul1) == 1
+    assert Enum.at(mul1, 0) === "mul(1,2)"
+
+    mul2 = Day3.find_mul("mul(1,2)mul(2,1)")
+    assert length(mul2) == 2
+    assert Enum.at(mul2, 0) === "mul(1,2)"
+    assert Enum.at(mul2, 1) === "mul(2,1)"
+
+    mul3 = Day3.find_mul("mul(2,1)mul(0,0)mul(-1,3)")
+    assert length(mul3) == 3
+    assert Enum.at(mul3, 0) === "mul(2,1)"
+    assert Enum.at(mul3, 1) === "mul(0,0)"
+    assert Enum.at(mul3, 2) === "mul(-1,3)"
+  end
+
+  test "mul calculates each multiply directive" do
+    assert Day3.mul(["mul(1,2)"]) === 2
   end
 end
