c46819efe3859b8fd03be6931b65d7067ddf583a
Garrett Phelps <garrphelps@gmail.com>
2024-12-09 21:34:58 -0600

    Day 4 - I hate grids

--- 2024/day4/lib/day4.ex
+++ 2024/day4/lib/day4.ex
@@ -7,12 +7,87 @@ defmodule Day4 do
   Hello world.
 
   ## Examples
-
-      iex> Day4.hello()
-      :world
-
   """
-  def hello do
-    :world
+  def create_grid("") do {0, 0, []} end
+  def create_grid(input) do
+    String.split(input, ~r/\r?\n/, trim: true)
+    |> Enum.map(fn element ->
+      String.graphemes(String.upcase(element))
+    end)
+  end
+
+  def count_word(search_term, word) do
+    Regex.scan(~r/#{search_term}/, word)
+    |> length()
+  end
+
+  def transpose(grid) do
+    grid
+    |> List.zip()
+    |> Enum.map(&Tuple.to_list/1)
+  end
+
+  def count_in_rows(grid, search_term \\ "XMAS") do
+    grid
+    |> Enum.map(&Enum.join/1)
+    |> Enum.map(fn row ->
+        count_word(search_term, row) + count_word(String.reverse(search_term), row)
+      end)
+    |> Enum.sum()
+  end
+
+  def count_diagonals(grid, search_term \\ "XMAS") do
+    get_diagonals(grid)
+    |> Enum.map(&Enum.join/1)
+    |> Enum.reduce(0, fn diagonal, acc ->
+      acc + count_word(search_term, diagonal) + count_word(String.reverse(search_term), diagonal)
+    end)
+  end
+
+  def get_diagonals(grid) do
+    height = length(grid)
+    width = length(hd(grid))
+
+    left_downward_right_diagonals = for offset <- -(height - 1)..(width - 1) do
+      for row <- 0..(height - 1),
+          col = row + offset,
+          col >= 0 and col < width,
+          do: Enum.at(Enum.at(grid, row), col)
+      end
+    |> Enum.reject(&(&1 == nil))
+
+    left_downward_left_diagonals = for offset <- 0..(height + width - 2) do
+      for row <- 0..(height - 1),
+          col = offset - row,
+          col >= 0 and col < width,
+          do: Enum.at(Enum.at(grid, row), col)
+      end
+    |> Enum.reject(&(&1 == nil))
+
+    left_downward_left_diagonals ++ left_downward_right_diagonals
+  end
+
+  def count_x_dash_mas(grid) do
+    height = length(grid)
+    width = length(hd(grid))
+    for row <- 1..(height - 2) do
+      for col <- 1..(width - 2) do
+        if Enum.at(Enum.at(grid, row), col) === "A" do
+          Enum.at(Enum.at(grid, row - 1), col - 1)
+          <> Enum.at(Enum.at(grid, row - 1), col + 1)
+          <> Enum.at(Enum.at(grid, row + 1), col - 1)
+          <> Enum.at(Enum.at(grid, row + 1), col + 1)
+        else
+          ""
+        end
+      end
+    end
+    |> Enum.flat_map(fn x -> x end)
+    |> Enum.reduce(0, fn x, acc ->
+      acc + count_word("MSMS", x) +
+        count_word("SMSM", x) +
+        count_word("SSMM", x) +
+        count_word("MMSS", x)
+    end)
   end
 end
--- 2024/day4/test/day4_test.exs
+++ 2024/day4/test/day4_test.exs
@@ -2,7 +2,97 @@ defmodule Day4Test do
   use ExUnit.Case
   doctest Day4
 
-  test "greets the world" do
-    assert Day4.hello() == :world
+  test "Input string is converted into a char grid" do
+    grid = Day4.create_grid("XMAS\nXMSA\nXAMS")
+    assert Enum.at(grid, 0) == ["X", "M", "A", "S"]
+    assert Enum.at(grid, 1) == ["X", "M", "S", "A"]
+    assert Enum.at(grid, 2) == ["X", "A", "M", "S"]
+  end
+
+  test "count_word counts the number of times a word occurs" do
+    assert Day4.count_word("XMAS", "XMASXMASsdfXMAS") == 3
+  end
+
+  test "count_in_rows finds horizontal matches" do
+    grid = [
+      [".", ".", "X", ".", ".", "."],
+      [".", "S", "A", "M", "X", "."],
+      [".", "A", ".", ".", "A", "."],
+      ["X", "M", "A", "S", ".", "S"],
+      [".", "X", ".", ".", ".", "."],
+    ]
+    assert Day4.count_in_rows(grid) == 2
+  end
+
+  test "count_in_rows finds multiple matches in a row" do
+    grid = [
+      ["X", "M", "A", "S", "X", "M", "A", "S"],
+    ]
+    assert Day4.count_in_rows(grid) == 2
+  end
+
+  test "count_in_rows finds vertical matches after transpose" do
+    grid = [
+      [".", ".", "X", ".", ".", "."],
+      [".", "S", "A", "M", "X", "."],
+      [".", "A", ".", ".", "A", "."],
+      ["X", "M", "A", "S", ".", "S"],
+      [".", "X", ".", ".", ".", "."],
+    ]
+    assert Day4.transpose(grid)
+    |> Day4.count_in_rows("XMAS") == 1
+  end
+
+  test "count_in_rows finds diagonal matches" do
+    grid = [
+      [".", ".", "X", ".", ".", "."],
+      [".", "S", "A", "M", "X", "S"],
+      [".", "A", ".", ".", "A", "."],
+      ["X", "M", "A", "M", ".", "S"],
+      [".", "X", "X", ".", ".", "."],
+    ]
+    assert Day4.count_diagonals(grid) == 2
+  end
+
+  test "count_x-mas finds matches" do
+    grid = [
+      [".", "M", ".", "S", ".", ".", ".", ".", ".", "."],
+      [".", ".", "A", ".", ".", "M", "S", "M", "S", "."],
+      [".", "M", ".", "S", ".", "M", "A", "A", ".", "."],
+      [".", ".", "A", ".", "A", "S", "M", "S", "M", "."],
+      [".", "M", ".", "S", ".", "M", ".", ".", ".", "."],
+      [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+      ["S", ".", "S", ".", "S", ".", "S", ".", "S", "."],
+      [".", "A", ".", "A", ".", "A", ".", "A", ".", "."],
+      ["M", ".", "M", ".", "M", ".", "M", ".", "M", "."],
+      [".", ".", ".", ".", ".", ".", ".", ".", ".", "."]
+    ]
+    assert Day4.count_x_dash_mas(grid) == 9
+  end
+
+  test "part1 integration" do
+    case File.read("test/test_data.txt") do
+      {:ok, content} ->
+        grid = Day4.create_grid(content)
+        horizontal = Day4.count_in_rows(grid)
+        vertical = Day4.transpose(grid)
+        |> Day4.count_in_rows()
+        diagonal = Day4.count_diagonals(grid)
+        assert horizontal + vertical + diagonal == 2336
+
+      {:error, reason} ->
+        raise "Error reading file: #{reason}"
+    end
+  end
+
+  test "part2 integration" do
+    case File.read("test/test_data.txt") do
+      {:ok, content} ->
+        grid = Day4.create_grid(content)
+        assert Day4.count_x_dash_mas(grid) == 1831
+
+      {:error, reason} ->
+        raise "Error reading file: #{reason}"
+    end
   end
 end
