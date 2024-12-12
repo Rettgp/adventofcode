b0e9d09883afc4dab61dd555d6b9197805fc3ed1
Garrett Phelps <garrphelps@gmail.com>
2024-12-11 21:51:45 -0600

    Day5 - Topological sort ftw

--- 2024/day5/lib/day5.ex
+++ 2024/day5/lib/day5.ex
@@ -5,16 +5,76 @@ defmodule Day5 do
 
   @doc """
   """
-  def specul_sorted?(data, rules) do
-    rule_map =
-      rules
-      |> Enum.map(fn [a, b] ->
-        {a, b}
-      end)
-      |> Map.new
+  def specul_sorted?(data, rule_map) do
+    satisfied? =
+      for i <- 1..length(data) - 1 do
+        rule = Map.get(rule_map, Enum.at(data, i))
+        {previous, _} = Enum.split(data, i)
+        rule_satisfied?(previous, rule)
+      end
 
-    IO.inspect(rule_map)
+    Enum.all?(satisfied?)
+  end
 
-    false
+  def find_middle_value(data) do
+    Enum.at(data, div(length(data), 2))
+  end
+
+  def parse_rules(content) do
+    content
+    |> String.split(~r/\r?\n/, trim: true)
+    |> Enum.map(fn val ->
+      String.split(val, "|")
+    end)
+    |> Enum.flat_map(fn val -> val end)
+    |> Enum.map(fn val -> String.to_integer(val) end)
+    |> Enum.chunk_every(2) # Create pairs of [key, value]
+    |> Enum.reduce(%{}, fn [key, value], acc ->
+      Map.update(acc, key, [value], fn existing -> existing ++ [value] end)
+    end)
+  end
+
+  def parse_rules_to_edges(content) do
+    content
+    |> String.split(~r/\r?\n/, trim: true)
+    |> Enum.map(fn val ->
+      String.split(val, "|")
+    end)
+    |> Enum.flat_map(fn val -> val end)
+    |> Enum.map(fn val -> String.to_integer(val) end)
+    |> Enum.chunk_every(2)
+    |> Enum.map(fn [val1, val2] ->
+      Graph.Edge.new(val1, val2)
+    end)
+  end
+
+  def parse_data(content) do
+    String.split(content, ~r/\r?\n/, trim: true)
+    |> Enum.map(fn line ->
+      String.split(String.trim(line), ",")
+    end)
+    |> Enum.map(fn inner_list ->
+      Enum.map(inner_list, &String.to_integer/1)  # Convert each string in the inner list to an integer
+    end)
+  end
+
+  defp rule_satisfied?([], _) do true end
+  defp rule_satisfied?(_, nil) do true end
+  defp rule_satisfied?(list, rule) do
+    !Enum.any?(rule, fn val -> Enum.member?(list, val) end)
+  end
+end
+
+defmodule SpechulSort do
+
+  def sort(list, edges) do
+    only_used_edges = Enum.reject(edges, fn edge ->
+      !Enum.member?(list, edge.v1) || !Enum.member?(list, edge.v2)
+    end)
+    graph = Graph.new
+    |> Graph.add_vertices(list)
+    |> Graph.add_edges(only_used_edges)
+
+    Graph.topsort(graph)
   end
 end
--- 2024/day5/test/day5_test.exs
+++ 2024/day5/test/day5_test.exs
@@ -3,7 +3,56 @@ defmodule Day5Test do
   doctest Day5
 
   test "spechul_sort can sort based on rules" do
-    rules = ["3|1", "2|4", "5|3", "5|2"]
-    assert Day5.specul_sorted?([2,1,3,5,4], rules) == true
+    rules = %{3=>[1], 2=>[4], 5=>[3, 2]}
+    assert Day5.specul_sorted?([5,3,1,2,4], rules) == true
+    assert Day5.specul_sorted?([5,3,2,1,4], rules) == true
+    assert Day5.specul_sorted?([5,3,2,4,1], rules) == true
+
+    assert Day5.specul_sorted?([5,1,3,2,4], rules) == false
+    assert Day5.specul_sorted?([2,5,3,1,4], rules) == false
+  end
+
+  test "find_middle_value gets the middle values in list" do
+    assert Day5.find_middle_value([5,3,1,2,4]) == 1
+    assert Day5.find_middle_value([5,3,2,1,4]) == 2
+    assert Day5.find_middle_value([5,1,3,2,4]) == 3
+  end
+
+  test "SpechulSort sorts based on rules" do
+    rules = "3|1\n2|4\n5|3\n5|2"
+    parsed_rules = Day5.parse_rules_to_edges(rules)
+    assert SpechulSort.sort([5,1,3,2,4], parsed_rules) == [5,2,4,3,1]
+    assert SpechulSort.sort([2,5,3,1,4], parsed_rules) == [5,2,4,3,1]
+  end
+
+  test "part1 integration" do
+    {:ok, rules_content} = File.read("test/rules.txt")
+    {:ok, data_content} = File.read("test/test_data.txt")
+
+    rules = Day5.parse_rules(rules_content)
+    data = Day5.parse_data(data_content)
+
+    result = Enum.reduce(data, 0, fn data_page, acc ->
+      if Day5.specul_sorted?(data_page, rules) do acc + Day5.find_middle_value(data_page) else acc end
+    end)
+
+    assert result == 143
+  end
+
+  test "part2 integration" do
+    {:ok, rules_content} = File.read("test/rules.txt")
+    {:ok, data_content} = File.read("test/test_data.txt")
+
+    edges = Day5.parse_rules_to_edges(rules_content)
+    rules = Day5.parse_rules(rules_content)
+    data = Day5.parse_data(data_content)
+
+    result = Enum.reduce(data, 0, fn data_page, acc ->
+      is_sorted = Day5.specul_sorted?(data_page, rules)
+      sorted = SpechulSort.sort(data_page, edges)
+      if !is_sorted do acc + Day5.find_middle_value(sorted) else acc end
+    end)
+
+    assert result == 4884
   end
 end
